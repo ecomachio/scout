398118054c4691395098e6de3e9c4b103fdb78b9
ecomachio <ediancomachio@hotmail.com>
2020-02-09 13:50:35 -0300

    competition ui v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,8 @@ const routes: Routes = [
   { path: 'choose-players', loadChildren: './choose-players/choose-players.module#ChoosePlayersPageModule' },
   { path: 'statistics', loadChildren: './statistics/statistics.module#StatisticsPageModule' },
   { path: 'player', loadChildren: './forms/player/player.module#PlayerPageModule' },
-  { path: 'category', loadChildren: './forms/category/category.module#CategoryPageModule' }
+  { path: 'category', loadChildren: './forms/category/category.module#CategoryPageModule' },  { path: 'competition', loadChildren: './forms/competition/competition.module#CompetitionPageModule' }
+
 ];
 
 @NgModule({
---
+++ src/app/entity/competition.ts
@@ -0,0 +1,11 @@
+export class Competition {
+
+    id: number;
+    name: string;
+    city: string;
+    start: Date;
+    end: Date;
+
+    constructor() { }
+
+}
