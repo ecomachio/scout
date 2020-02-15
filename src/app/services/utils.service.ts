fcde705a321e1c072273049ea42dbdf12e88b202
ecomachio <ediancomachio@hotmail.com>
2020-02-15 17:55:26 -0300

    created utils service

---
+++ src/app/services/utils.service.ts
@@ -0,0 +1,19 @@
+import { ToastController } from '@ionic/angular';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UtilsService {
+
+    constructor(private toastController: ToastController) {
+
+    }
+
+    showToast(msg) {
+        this.toastController.create({
+            message: msg,
+            duration: 2000
+        }).then(toast => toast.present());
+    }
+}
