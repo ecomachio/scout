72dee89d2a6eef87b723bcf06b8f421e4803a923
Edian Comachio <edian.comachio@moduscreate.com>
2021-04-17 17:09:41 -0300

    prettier

--- src/app/forms/competition/competition.page.ts
+++ src/app/forms/competition/competition.page.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Competition } from 'src/app/entity/competition';
-import { Match } from 'src/app/entity/match';
-import { PickerController, NavController, LoadingController } from '@ionic/angular';
-import { MatchService } from 'src/app/services/match.service';
-import { CompetitionService } from 'src/app/services/competition.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UtilsService } from 'src/app/services/utils.service';
+import { Component, OnInit } from "@angular/core";
+import { Competition } from "src/app/entity/competition";
+import { Match } from "src/app/entity/match";
+import {
+  PickerController,
+  NavController,
+  LoadingController,
+} from "@ionic/angular";
+import { MatchService } from "src/app/services/match.service";
+import { CompetitionService } from "src/app/services/competition.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { UtilsService } from "src/app/services/utils.service";
 
 @Component({
-  selector: 'app-competition',
-  templateUrl: './competition.page.html',
-  styleUrls: ['./competition.page.scss'],
+  selector: "app-competition",
+  templateUrl: "./competition.page.html",
+  styleUrls: ["./competition.page.scss"],
 })
 export class CompetitionPage implements OnInit {
-
   competition: Competition;
 
   constructor(
@@ -25,7 +28,7 @@ export class CompetitionPage implements OnInit {
     private loadingController: LoadingController,
     private utilsService: UtilsService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.competition = new Competition();
@@ -37,10 +40,10 @@ export class CompetitionPage implements OnInit {
 
   async loadCompetition(competitionId) {
     const loading = await this.loadingController.create({
-      message: 'Loading Competition..'
+      message: "Loading Competition..",
     });
     await loading.present();
-    this.competitionService.getCompetition(competitionId).subscribe(res => {
+    this.competitionService.getCompetition(competitionId).subscribe((res) => {
       loading.dismiss();
       console.log(res);
       this.competition = res.data() as Competition;
@@ -52,26 +55,29 @@ export class CompetitionPage implements OnInit {
     if (this.competition.id) {
       await this.competitionService.removeCompetition(this.competition.id);
     }
-    this.utilsService.showToast('Aluno excluído');
-    this.router.navigateByUrl('/matches');
+    this.utilsService.showToast("Aluno excluído");
+    this.router.navigateByUrl("/matches");
   }
 
   async done() {
     this.save();
-    this.router.navigateByUrl('/competitions');
+    this.router.navigateByUrl("/competitions");
   }
 
   async save() {
     if (this.competition.id) {
-      return await this.competitionService.updateCompetition(this.competition, this.competition.id);
+      return await this.competitionService.updateCompetition(
+        this.competition,
+        this.competition.id
+      );
     } else {
       return await this.competitionService.addCompetition(this.competition);
     }
-    this.utilsService.showToast('Pronto');
+    this.utilsService.showToast("Pronto");
   }
 
   async navToMatchs() {
-    const res = await this.save() as any;
+    const res = (await this.save()) as any;
 
     if (!this.competition.id) {
       this.competition.id = res.id;
@@ -80,5 +86,4 @@ export class CompetitionPage implements OnInit {
     console.log(this.competition);
     this.router.navigateByUrl(`/matches/${this.competition.id}`);
   }
-
 }
